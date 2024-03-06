b8ed6c43d2627521708e1a07cb9913b982f514ce
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-07 04:13:29 +0500

    aksiya section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import { useGetTel } from '../../service/query/useGetTel'
 import Slider from '../../components/Slider'
+import Img1 from '../../assets/photo/aksiya1.png'
+import Img2 from '../../assets/photo/aksiya2.png'
+import Img3 from '../../assets/photo/aksiya3.png'
 
 const Home = () => {
   const {data} = useGetTel()
@@ -12,6 +15,16 @@ const Home = () => {
       <div className='max-w-[1400px] m-auto'>
         <h2 className='mb-6 font-semibold text-xl'>Смартфоны и планшеты</h2>
         <Slider data={data} />
+      </div>
+      <div className='bg-[#FEEE00] my-8 pb-8'>
+        <div className='max-w-[1400px] m-auto'>
+        <h3 className='text-3xl font-semibold pt-4 pb-6'>Акции</h3>
+        <div className='flex justify-between'>
+          <img src={Img1} alt="" />
+          <img src={Img2} alt="" />
+          <img src={Img3} alt="" />
+        </div>
+        </div>
       </div>
       
     </div>
