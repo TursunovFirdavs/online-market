ec146f56c47bb060a512f0a4e32912337d91c5d8
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-06 12:17:33 +0500

    category added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,9 +6,12 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { UeseGetCategory } from '../service/query/useGetCategory';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     const { data, isLoading } = useGetBanner()
+    const { data: category,  } = UeseGetCategory()
     console.log(data);
   return (
     <div className='w-full h-[300px]'>
@@ -34,6 +37,15 @@ const Banner = () => {
         
         
       </Swiper>
+        <div className='flex justify-between max-w-[1400px] m-auto my-8'>
+        {
+          category?.map(item => (
+            <Link>
+              <img src={item.img} alt="" />
+            </Link>
+          ))
+        }
+        </div>
     </div>
   )
 }
