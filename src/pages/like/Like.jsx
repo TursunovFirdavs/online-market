bbecbe476c82745a6e16a5c830676cebe117d3ca
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-11 14:35:06 +0500

    like page ui

--- src/pages/like/Like.jsx
+++ src/pages/like/Like.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Card from '../../components/Card';
 
 const Like = () => {
+  const { like } = useSelector(state => state.like)
+  console.log(like);
   return (
-    <div>Like</div>
+    <div>
+      {like.length &&
+        <div className='max-w-[1400px] m-auto'>
+          <h2 className='mb-6 font-semibold text-3xl mt-4'>Избранные товары</h2>
+          <div className='flex gap-x-[100px] my-[50px] gap-y-[50px] flex-wrap'>
+            {
+              like?.map(item => <Card {...item} />)
+            }
+          </div>
+        </div>
+      }
+    </div>
   )
 }
 
