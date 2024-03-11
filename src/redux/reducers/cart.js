5112ba78fffe5d1ef3d03a820e6ed2d715b73741
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-11 15:26:23 +0500

    add to cart reducer

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: []
+}
+
+const cartReducer = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const checked = state.cart.find(item => item.id === action.payload.id)
+            if(!checked){
+                return {...state, cart: [state.cart,
+                    {
+                        ...action.payload,
+                        productCount: 1,
+                        overalPrice: action.payload.price
+                    }
+                ]}
+            }
+        },
+        removeFromCart: (state, action) => {
+            const checked = state.cart.filter(item => item.id !== action.payload.id)
+            return {...state, cart: checked}
+        }
+    }
+})
+
+export default cartReducer.reducer
+
+export const { addToCart, removeFromCart } = cartReducer.actions
